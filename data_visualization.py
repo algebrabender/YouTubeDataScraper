505d77472b583f4261d8f1078ca3eaf3ed600e71
algebrabender <jelena.firulovic@gmail.com>
2022-08-19 16:15:30 +0200

    update 19.8. tooltipl for line graphs, work in progress

--- data_visualization.py
+++ data_visualization.py
@@ -1,3 +1,4 @@
+from operator import is_
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -36,16 +37,20 @@ def show_graphs() -> None:
     fig.canvas.manager.set_window_title('Girls Camerwork Guide Video Stats')
     fig.suptitle("Girls Camerwork Guide Video Stats", fontsize="xx-large")
 
-    axs[0, 0].scatter(x, y1, color="red")
+    plot,  = axs[0, 0].plot(x, y1, color="red", marker='o')
     axs[0, 0].set_xlabel("Date")
-    axs[0, 0].set_ylabel("Views Count")   
+    axs[0, 0].set_ylabel("Views Count")  
+    annotation = axs[0, 0].annotate(text='', xy=(0, 0), xytext=(10, 10),textcoords='offset points', 
+                                    bbox={'boxstyle': 'round', 'fc': 'w'},
+                                    arrowprops={'arrowstyle': '->'}) 
+    annotation.set_visible(False)
 
-    axs[0, 1].scatter(x, y2, color="green")
+    axs[0, 1].plot(x, y2, color="green")
     axs[0, 1].set_xlabel("Date")
     axs[0, 1].set_ylabel("Comments Count")
     #axs[1].yaxis.set_major_locator(ticker.MultipleLocator(1))
 
-    axs[0, 2].scatter(x, y3, color="blue")
+    axs[0, 2].plot(x, y3, color="blue")
     axs[0, 2].set_xlabel("Date")
     axs[0, 2].set_ylabel("Likes Count")
 
@@ -71,4 +76,26 @@ def show_graphs() -> None:
     for ax in axs[1]:
         ax.ticklabel_format(axis='y', useOffset=False, style='plain')
 
+    def update_annotation(ind):
+        x,y = plot.get_data()
+        annotation.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
+        text = "{}, {}".format(x[ind["ind"][0]], y[ind["ind"][0]])
+        annotation.set_text(text)
+
+    def hover(event):
+        annotation_visibility = annotation.get_visible()
+        if event.inaxes == axs[0, 0]:
+            is_contained, annotation_index = plot.contains(event)
+            if is_contained:
+                update_annotation(annotation_index)
+                annotation.set_visible(True)
+                fig.canvas.draw_idle()
+            else:
+                if annotation_visibility:
+                    annotation.set_visible(False)
+                    fig.canvas.draw_idle()
+
+    fig.canvas.mpl_connect('motion_notify_event', hover)
+
     plt.show()
+    #return fig
